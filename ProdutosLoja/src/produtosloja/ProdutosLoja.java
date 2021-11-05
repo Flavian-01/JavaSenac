b39a4559f34896b815a5489f21e048f101daf110
Administrador <Administrador@CAML192997>
2021-11-04 21:30:21 -0300

    Produtos da loja em pretacoes (foi adicionado comentarios)

--- ProdutosLoja/src/produtosloja/ProdutosLoja.java
+++ ProdutosLoja/src/produtosloja/ProdutosLoja.java
@@ -9,15 +9,20 @@ public class ProdutosLoja {
  
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
+        
+        //Declarando variáveis
         double compra;       
         int quantPrestacao = 5;
         double valorPrestacao ;
         
+        //Mensagem mostrada para o usuário
         System.out.println("Digite o valor da sua compra: ");
-            compra = input.nextDouble();
-            
+            compra = input.nextDouble();  //irá ler o que o usuário digitar
+        
+        //Calculo para saber o valor da prestação
         valorPrestacao = compra/ quantPrestacao;
         
+        //Mensagem final para o usuário mostrando o resultado
         System.out.println("O valor da parcela é: "+quantPrestacao+"x de R$ "+ valorPrestacao+" Reais");
     }
     
