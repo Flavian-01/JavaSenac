800f152806a428ee9b9c3cd4b9b45375da885ab7
Administrador <Administrador@CAML192997>
2021-11-08 20:00:33 -0300

    exemplo if e else

---
+++    ExemploIf_Else/src/exemploif_else/ExemploIf_Else.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exemploif_else;
+
+public class ExemploIf_Else {
+
+    public static void main(String[] args) {
+        boolean var1 = true;
+        //Se var1 for verdadeiro
+        if (var1) {
+            System.out.println("var1: Verdadeiro");
+        } else {
+            System.out.println("var1: Falso");
+        }
+        
+        // Se var1 for falso 
+        if (!var1) {
+            System.out.println("!var1: Verdadeiro");
+        } else {
+            System.out.println("!var1: Falso");
+
+        }
+       
+    }
+
+}
