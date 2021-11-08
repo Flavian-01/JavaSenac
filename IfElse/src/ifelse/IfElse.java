6bb3657b3955a84dc8ca3d1b356646e54422ae54
Administrador <Administrador@CAML192997>
2021-11-08 20:17:39 -0300

    outro exemplo if else

---
+++ IfElse/src/ifelse/IfElse.java
@@ -0,0 +1,31 @@
+
+package ifelse;
+
+
+public class IfElse {
+
+ 
+    public static void main(String[] args) {
+        int idade = 85;
+        
+        if (idade <= 40)
+        {
+            System.out.println("Menor que 40");
+        }
+        else if (idade <= 60)
+        {
+            System.out.println("Menor que 60");
+        }
+        else if (idade <= 80)
+        {
+            System.out.println("Menor que 80");
+        }
+        else if (idade <= 100)
+        {
+            System.out.println("Menor que 100");
+        
+        
+        }
+    }
+    
+}
