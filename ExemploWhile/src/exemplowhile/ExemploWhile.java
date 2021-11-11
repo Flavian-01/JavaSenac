8f3c7d6b527af804d1b025d2fe97022df1f5bce8
Administrador <Administrador@CAML192997>
2021-11-11 19:51:17 -0300

    Exemplo while com if

---
+++ ExemploWhile/src/exemplowhile/ExemploWhile.java
@@ -0,0 +1,17 @@
+
+package exemplowhile;
+
+
+public class ExemploWhile {
+
+   
+    public static void main(String[] args) {
+       int contador = 0;
+       
+       while (contador <= 50) {
+           System.out.println("Repetição nr: "+ contador);
+           contador++;
+       }
+    }
+    
+}
