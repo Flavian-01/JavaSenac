05989ebefa7a03174dd8ad0164cf6965a079bfc3
Administrador <Administrador@CAML192997>
2021-11-11 21:30:41 -0300

    exemplo for tabuada

---
+++ Tabuada/src/tabuada/Tabuada.java
@@ -0,0 +1,17 @@
+
+package tabuada;
+
+
+public class Tabuada {
+
+    
+    public static void main(String[] args) {
+       int numero = 0;
+       for (numero = 1; numero <=10; numero++) 
+       {
+        System.out.println("Tabuada: 1 x "+ numero + " = "+ 1*numero);
+       }
+       
+    }
+    
+}
