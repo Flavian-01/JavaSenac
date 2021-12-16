4a01ebfdf143bd99bee846f6b19eb50c4af47453
Administrador <Administrador@CAML192997>
2021-12-16 19:44:40 -0300

    ExemploBreak

---
+++ ExemploBreak/src/exemplobreak/ExemploBreak.java
@@ -0,0 +1,15 @@
+
+package exemplobreak;
+
+
+public class ExemploBreak {
+
+    public static void main(String[] args) {
+       for (int contador=1; contador <= 1000; contador++){
+           System.out.println("Esta é a repetição nr: "+contador);
+           if (contador==10)
+               break;
+       }
+    }
+    
+}
