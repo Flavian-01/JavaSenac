d3c1b4184d676af65dcb7d88863145794868a071
Administrador <Administrador@CAML192997>
2021-12-20 18:44:44 -0300

    Exemplo funcao

---
+++ ExemploFuncao/src/exemplofuncao/ExemploFuncao.java
@@ -0,0 +1,19 @@
+
+package exemplofuncao;
+
+
+public class ExemploFuncao {
+
+    //criando a função
+    public static void mostrarMensagem(){
+        System.out.println("Minha Mensagem");
+    }
+   
+    public static void main(String[] args) {
+        
+        //chamando a função do programa
+        mostrarMensagem();
+        
+    }
+    
+}
