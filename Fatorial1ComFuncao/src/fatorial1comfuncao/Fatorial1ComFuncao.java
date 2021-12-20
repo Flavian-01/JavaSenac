0dac59a9242da8e474f7bd8a633aedb4ade424cd
Administrador <Administrador@CAML192997>
2021-12-20 18:56:40 -0300

    fatorial com funcao

---
+++ Fatorial1ComFuncao/src/fatorial1comfuncao/Fatorial1ComFuncao.java
@@ -0,0 +1,21 @@
+
+package fatorial1comfuncao;
+
+
+public class Fatorial1ComFuncao {
+
+    public static void fatorar(int numero){
+        int fator = 1;
+        for (int  i = numero; i > 1; i--){
+        fator *= i;
+    }
+        System.out.println(numero +"! = "+ fator );
+    }
+    
+    public static void main(String[] args) {
+        for (int x = 1; x<=10; x++)
+            fatorar (x);
+        
+    }
+    
+}
