e00f49b62268fd179b03f97a52ecad7aaa139857
Administrador <Administrador@CAML192997>
2021-12-20 19:09:37 -0300

    primo

---
+++ Primo/src/primo/Primo.java
@@ -0,0 +1,26 @@
+
+package primo;
+
+
+public class Primo {
+
+    public static boolean ehPrimo (long nr){
+        if (nr < 2 )
+            return false;
+        for (long i = 2; i <= (nr / 2); i++){
+            if (nr % i == 0)
+                return false;
+        }
+         return true;
+    }
+ 
+    public static void main(String[] args) {
+        long x = 5;
+        if (ehPrimo(x)) // se for primo
+            System.out.println(x +" é primo");
+        else //se não for primo
+            System.out.println(x + " não é primo");
+       
+    }
+    
+}
