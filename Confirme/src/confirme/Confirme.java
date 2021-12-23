ebb8cb6b6ae721ef815c65bb403bf2fd45798f22
Administrador <Administrador@CAML192997>
2021-12-23 18:45:41 -0300

    confirme

---
+++ Confirme/src/confirme/Confirme.java
@@ -0,0 +1,13 @@
+
+package confirme;
+
+import javax.swing.JOptionPane;
+
+
+public class Confirme {
+
+    public static void main(String[] args) {
+        JOptionPane.showConfirmDialog(null,"Escolha um: ", "Escolha um", JOptionPane.YES_NO_OPTION);
+    }
+    
+}
