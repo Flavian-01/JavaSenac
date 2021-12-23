11b94239c557f39c482188466ec311eb12d83bec
Administrador <Administrador@CAML192997>
2021-12-23 19:59:37 -0300

    Teste Wrappers2

---
+++ TesteWrapper2/src/testewrapper2/TesteWrapper2.java
@@ -0,0 +1,17 @@
+
+package testewrapper2;
+
+public class TesteWrapper2 {
+
+    public static void main(String[] args) {
+       //String
+       String num = "10";
+       
+       //String - Primitivo
+       
+       double d = Double.parseDouble("10");
+        System.out.println("Esse é um número: "+ d);
+        System.out.println("Esse é um String: "+ num);
+    }
+    
+}
