921a1fc5ef7c1a275dec5d305dce70832d49fe9a
Administrador <Administrador@CAML192997>
2021-12-23 18:50:00 -0300

    JOptionPane exemplo 2

---
+++ InoutDialog/src/inoutdialog/InoutDialog.java
@@ -0,0 +1,14 @@
+
+package inoutdialog;
+
+import javax.swing.JOptionPane;
+
+
+public class InoutDialog {
+
+   
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Conhecendo JAVA","JAVA", JOptionPane.WARNING_MESSAGE );
+    }
+    
+}
