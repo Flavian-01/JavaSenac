10e257eae09f5dc1eb1429484cf341d39d2042c4
Administrador <Administrador@CAML192997>
2021-12-23 18:51:21 -0300

    JOptionPane exemplo 3

---
+++ Mensagem/src/mensagem/Mensagem.java
@@ -0,0 +1,14 @@
+
+package mensagem;
+
+import javax.swing.JOptionPane;
+
+
+public class Mensagem {
+
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Curso técnico em informática");
+    }
+    
+}
