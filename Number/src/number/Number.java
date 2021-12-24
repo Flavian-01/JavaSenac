992c197465b53694f10d7970e939d520ebaa0c3e
Administrador <Administrador@CAML192997>
2021-12-23 21:01:38 -0300

    number

---
+++ Number/src/number/Number.java
@@ -0,0 +1,17 @@
+
+package number;
+
+
+public class Number {
+
+  
+    public static void main(String[] args) {
+      int resultado;
+      String numString = "20";
+      int n1 = 2;
+      int n2 = Integer.parseInt(numString);//transforma o número em String em int
+      resultado = n1/n2;
+        System.out.println("Resultado = "+ resultado);
+    }
+    
+}
